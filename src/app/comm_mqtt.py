b8aa3ceb3a09f245372671840d0e2a01350474df
KyleVL_NU <kylel@nu.ac.th>
2022-12-21 10:49:14 +0700

    Have the program running stably, with just the status indicators, and a run button.  Not fully documented yet.

--- src/app/comm_mqtt.py
+++ src/app/comm_mqtt.py
@@ -13,34 +13,49 @@ import time
 # installed library
 import paho.mqtt.client as mqtt
 
+# local file
+import main_gui  # for type hinting
+
 HIVEMQTT_PORT = 1883  # CONSTANT
 HIVEMQTT_BROKER = "broker.hivemq.com"
 PUBLISH_TOPIC = "Naresuan/Kyle"
 SUBSCRIBE_TOPIC = "Naresuan/+"
 
 
-def on_subscription(*args):
-    print("subscribed: ", args)
+class MQTTConn:
+    def __init__(self, root: main_gui.SensorUI):
+        self.root = root
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connection
+        self.client.on_message = self.on_message
+        self.client.connect(HIVEMQTT_BROKER, HIVEMQTT_PORT)
+        self.client.loop_start()
+
+    def publish(self, message):
+        self.client.publish(PUBLISH_TOPIC, message)
+
+    def on_connection(self, *args):
+        """ Call back for when mqtt connects to the broker
+         and prints out an acknowledgement and subscribes """
+        print("Connected")
+        self.client.subscribe(SUBSCRIBE_TOPIC)
+
+    def on_message(self, client, user_data, msg: mqtt.MQTTMessage):
+        print("got message: ", msg.payload)
+        print("from topic: ", msg.topic)
+        name = msg.topic.split('/')[-1]
+        print("message from: ", name)
+        if msg.payload == b"On":
+            sensor_state = True
+        else:
+            sensor_state = False
+        self.root.change_status(name,
+                                sensor_state)
 
 
-def on_connection(*args):
-    """ Call back for when mqtt connects to the broker
-     and prints out an acknowledgement and subscribes """
-    print("Connected")
-    client.subscribe(SUBSCRIBE_TOPIC)
-
-
-def on_message(client, user_data, msg: mqtt.MQTTMessage):
-    print("got message: ", msg.payload)
-
-
-client = mqtt.Client()
-client.on_connect = on_connection
-client.on_subscribe = on_subscription
-client.on_message = on_message
-client.connect(HIVEMQTT_BROKER, HIVEMQTT_PORT)
-client.loop_start()
-while True:
-    client.publish(PUBLISH_TOPIC,
-                   "hello this is Kyle")
-    time.sleep(10)
+if __name__ == '__main__':
+    client = MQTTConn()
+    while True:
+        client.publish(PUBLISH_TOPIC,
+                       "hello this is Kyle")
+        time.sleep(10)
