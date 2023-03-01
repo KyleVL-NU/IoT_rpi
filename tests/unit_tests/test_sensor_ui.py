18e0e592d0f718d0284d3480c09cab1178257560
KyleVL_NU <kylel@nu.ac.th>
2023-03-01 12:01:21 +0700

    Unittests the UI class

--- tests/unit_tests/test_sensor_ui.py
+++ tests/unit_tests/test_sensor_ui.py
@@ -32,7 +32,7 @@ class TestSensorUI(unittest.TestCase):
             gui.button_click()
             gui.comm.publish.assert_called()
             gui.comm.publish: mock.MagicMock  # type hinting
-            gui.comm.publish.assert_called_with("On")
+            gui.comm.publish.assert_called_with("On", is_data=False)
 
 
 if __name__ == '__main__':
