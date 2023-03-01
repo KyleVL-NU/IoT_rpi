994833902086542eadedfe87f437cb873a683792
KyleVL_NU <kylel@nu.ac.th>
2023-03-01 12:00:39 +0700

    Unittests to check data passes from the data class to the display class

--- tests/integration_tests/test_data_to_display.py
+++ tests/integration_tests/test_data_to_display.py
@@ -13,6 +13,7 @@ from datetime import datetime
 import os
 import sys
 import unittest
+from unittest import mock
 
 sys.path.append(os.path.join("..", "..", "src", "app"))
 # local files
@@ -26,15 +27,26 @@ class TestDataToDisplay(unittest.TestCase):
         Test that adding data to the data class makes the
         pyplot graph have the correct xy data
         """
-        sensor_class = sensor_data.SensorData(None)
+        sensor_class = sensor_data.SensorHubData(None)
+        print(f"START {sensor_class.display.lines['device 2'].get_xydata()}")
         # add 1 data point and check it is added correctly
         now = datetime(2023, 1, 24, 10, 50, 12)
-        sensor_class.add_data(now, 5)
-        xy_data = sensor_class.display.lines[0].get_xydata().tolist()
+        sensor_class.add_data("device 2", now, 5)
+        print("==")
+        print(sensor_class.display.lines)
+        xy_data = sensor_class.display.lines["device 2"].get_xydata().tolist()
         # TODO: calculate how datetime is converted to a float
         self.assertListEqual([[19381.45152777778, 5.0]], xy_data)
 
-
+    def test_draw_is_called(self):
+        # initialize the sensor class
+        sensor_class = sensor_data.SensorHubData(None)
+        # to check if a method is called mock it first
+        sensor_class.display.canvas.draw = mock.Mock()
+        now = datetime(2023, 1, 24, 10, 50, 12)
+        sensor_class.add_data("device 2", now, 5)
+        # check that the draw function is called
+        sensor_class.display.canvas.draw.assert_called()
 
 
 if __name__ == '__main__':
